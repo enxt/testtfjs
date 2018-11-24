be2cc56c88fc617337c8decc9d6fda383ea5e87e
efernandez <efernandez>
2018-11-24 10:41:24 +0100

    Added index.js for the module classes load

---
+++ index.js
@@ -0,0 +1,12 @@
+const Agent = require("./agent"),
+      Trainer = require("./trainer"),
+      Utils = require("./utils"),
+      Environment = require("./environment");
+
+
+module.exports = {
+    Agent,
+    Trainer,
+    Utils,
+    Environment
+};
